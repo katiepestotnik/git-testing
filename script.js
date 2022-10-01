2d9c367bfc36553b0d1bc7ad518545d1e1374c86
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-10-01 17:31:03 -0600

    hard to break

--- script.js
+++ script.js
@@ -6,4 +6,4 @@ const branch = console.log('this is a branch')
 const conflict = true
 console.log('deleted and added back')
 
-console.log('creating conflict')
+console.log('creating conflict hardly')
