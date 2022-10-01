12b6ddad6ec4d23e1d2ebf54c9957622b4b5a4e6
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-10-01 17:26:53 -0600

    add conflict

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('git testing')
 console.log('this is the branching branch')
 
 const branch = console.log('this is a branch')
-
+const conflict = true
 console.log('deleted and added back')
 
 console.log('creating conflict')
